421434ba5cb0f3b836982eee047cf43d24cb5777
VelkovIv <ivaylo.velkov@gmail.com>
2023-01-04 21:48:23 +0200

    updates

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-var randomNumber1 = Math.random() * 6;
-randomNumber1 = Math.floor(randomNumber1) + 1;
-
-
-
 
 var randomNumber2 = Math.random() * 6;
+var diceOne = document.getElementById("dice1");
+var diceTwe = document.getElementById("dice2");
+
+function changeDices(){
+    var randomNumber1 = Math.random() * 6;
+    randomNumber1 = Math.floor(randomNumber1) + 1;
+    diceOne.setAttribute("src", "images/dice" + randomNumber1 + ".png")
+}
+
+
+
+
+
+
 randomNumber2 = Math.floor(randomNumber2) + 1;
