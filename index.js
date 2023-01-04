72e4eef89ad7d932ad37d79eb84541bed07ca923
VelkovIv <ivaylo.velkov@gmail.com>
2023-01-04 22:08:32 +0200

    Updates

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-
 var diceOne = document.getElementById("dice1");
-var diceTwe = document.getElementById("dice2");
+var diceTwo = document.getElementById("dice2");
 
-function changeDices(){
-    var randomNumber1 = Math.random() * 6;
-    var randomNumber2 = Math.random() * 6;
-    randomNumber1 = Math.floor(randomNumber1) + 1;
-    var randomDiceImage1 = "images/dice" + randomNumber1 + ".png"
-    diceOne.setAttribute("src", randomDiceImage1)
+var randomNumber1 = Math.random() * 6;
+randomNumber1 = Math.floor(randomNumber1) + 1;
+diceOne.setAttribute("src", "images/dice" + randomNumber1 + ".png")
 
-    randomNumber2 = Math.floor(randomNumber2) + 1;
+
+var randomNumber2 = Math.random() * 6;
+randomNumber2 = Math.floor(randomNumber2) + 1;
+diceTwo.setAttribute("src", "images/dice" + randomNumber2 + ".png")
+
+if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 Wins!";
 }
-
-
-
-
-
-
+else if (randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 2 Wins!"
+} else{
+    document.querySelector("h1").innerHTML = "Draw!"
+}
