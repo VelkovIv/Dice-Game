9c1256834ad02c6cf78586f90491f1006c50f27e
VelkovIv <ivaylo.velkov@gmail.com>
2023-01-04 21:02:40 +0200

    JS update

---
+++ index.js
@@ -0,0 +1,8 @@
+var randomNumber1 = Math.random() * 6;
+randomNumber1 = Math.floor(randomNumber1) + 1;
+
+
+
+
+var randomNumber2 = Math.random() * 6;
+randomNumber2 = Math.floor(randomNumber2) + 1;
